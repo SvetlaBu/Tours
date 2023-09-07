542908470a3a5443ff8fbc385373a19dd9a81e5d
Sveta_Burakova <svetlaburs@gmail.com>
2023-09-07 11:24:23 +0300

    Исправляет баги

--- source/js/modules/hero/slider.js
+++ source/js/modules/hero/slider.js
@@ -1,9 +1,11 @@
 import Swiper from '../../vendor/swiper';
 import {isMobile} from '../../utils/is-mobile';
 
+let slide = document.querySelector('.hero__item');
+
 const addHeroSlider = () => {
   if (document.querySelector('.hero__slider')) {
-    return new Swiper('.hero__slider', {
+    const swiper = new Swiper('.hero__slider', {
       allowTouchMove: isMobile(),
       pagination: {
         el: '.hero__pagination',
@@ -13,6 +15,17 @@ const addHeroSlider = () => {
         },
       },
       loop: true,
+    });
+    swiper.on('slideChange', function () {
+      if (document.querySelector('iframe')) {
+        let iframes = document.querySelectorAll('iframe');
+        for (let i = 0; i < iframes.length; i++) {
+          if (iframes[i] !== null) {
+            let temp = iframes[i].src;
+            iframes[i].src = temp;
+          }
+        }
+      }
     });
   }
   return null;
--- source/js/modules/hero/video.js
+++ source/js/modules/hero/video.js
@@ -3,7 +3,7 @@ const addVideoPlayer = () => {
     const root = document.querySelector('.video');
     const button = root.querySelector('button');
     const iframe = `
-  <iframe width="100%" height="100%" src="https://www.youtube.com/embed/${button.dataset.src}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  <iframe id='video' width="100%" height="100%" src="https://www.youtube.com/embed/${button.dataset.src}?autoplay=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   `;
 
     button.addEventListener('click', () => {
